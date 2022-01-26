f9726167be39c945b078bbade468c49ffca659bd
minch-yoda <minchenko.dev@gmail.com>
2022-01-26 10:48:37 +0300

    all done but there is a problem with IR leds I have, they hardly light up if at all :/ maybe mc isn't powerful enough to power them up, idk.

--- photoshutter.c
+++ photoshutter.c
@@ -9,6 +9,79 @@ ATtiny2313 8 MHz
 #include <stdio.h>
 #include <stdlib.h>
 
+const int x0 = 24;
+const int x1 = 48;
+
+//1s/40kHz=25us total (6+19) pulse (microseconds)
+//600us/25us=24 cycles
+void pulse(int cycles){
+    int c = 0;
+    while(c<cycles){
+        c++;
+        PORTB=0b11111111;
+        PORTD.5 = 1;
+        PORTD.4 = 1; 
+        PORTD.3 = 1;
+        PORTD.2 = 1;
+        delay_us(10);
+        PORTB=0b00000000;
+        PORTD.5 = 0;
+        PORTD.4 = 0; 
+        PORTD.3 = 0;
+        PORTD.2 = 0;
+        delay_us(15);
+    }
+    delay_us(600);
+}
+
+void shutter(){
+    pulse(x1);
+    pulse(x0);
+    pulse(x1);
+    pulse(x1);
+    pulse(x0);
+    pulse(x1);
+    pulse(x0);
+}
+
+void shutter2sec(){
+    pulse(x1);
+    pulse(x1);
+    pulse(x0);
+    pulse(x1);
+    pulse(x1);
+    pulse(x1);
+    pulse(x0);
+}
+
+void videobutton(){
+    pulse(x1);
+    pulse(x0);
+    pulse(x0);
+    pulse(x1);
+    pulse(x0);
+    pulse(x0);
+    pulse(x0);
+}
+
+void address(){
+    pulse(x0);
+    pulse(x1);
+    pulse(x0);
+    pulse(x1);
+    pulse(x1);
+    pulse(x1);
+    pulse(x0);
+    pulse(x0);
+    pulse(x0);
+    pulse(x1);
+    pulse(x1);
+    pulse(x1);
+    pulse(x1);
+}
+
+
+/*
 void delay_second(int n){
     while(n--){
         delay_ms(1000);
@@ -39,10 +112,11 @@ void delay_day(int n){
 void delay_shot(){
       delay_ms(200);   //for camera to take a shot
 }
+*/
 void main(void)
 {
 // Declare your local variables here
-int unit_count = 0;
+//int unit_count = 0;
 
 // Crystal Oscillator division factor: 1
 #pragma optsize-
@@ -67,8 +141,7 @@ DDRB=0b00000000;
 PORTB=0b00000000;
 
 // Port D
-//All input
-//  CLCK  day  hour  min  sec  UNUSED UNUSED
+// CLCK  day  hour  min  sec  UNUSED UNUSED
 DDRD=0b00000000;
 //Should be low
 PORTD=0b00000000;
@@ -76,6 +149,11 @@ PORTD=0b00000000;
 PORTA.0 = 0;
 delay_ms(200);
 
+
+
+
+//this SHIT SDOESNT'T WORK
+/*
 unit_count = 0;
 if(PINB.0 == 1){unit_count = 1;}
 if(PINB.1 == 1){unit_count = 2;}
@@ -89,48 +167,63 @@ if(unit_count==0){
     unit_count=1; //no pins
 }
 
-//this SHIT SDOESNT'T WORK
-//if(PIND.4 == 1){ //minutes
-//    while(1){
-//        //take photo    
-//        PORTA.0 = 1;
-//        delay_shot();
-//        PORTA.0 = 0;
-//        delay_minute(unit_count);
-//    }
-//} else if(PIND.3 == 1){ //hours
-//    while(1){
-//        //take photo    
-//        PORTA.0 = 1;
-//        delay_shot();
-//        PORTA.0 = 0;
-//        delay_hour(unit_count);
-//    }
-//} else if(PIND.2 == 1){ //days
-//    while(1){
-//        //take photo    
-//        PORTA.0 = 1;
-//        delay_shot();
-//        PORTA.0 = 0;
-//        delay_day(unit_count);
-//    }
-//}  else {  //seconds
-//    while(1){
-//        //take photo    
-//        PORTA.0 = 1;
-//        delay_shot();
-//        PORTA.0 = 0;
-//        delay_second(unit_count);
-//    }
-//}
+
+
+if(PIND.4 == 1){ //minutes
     while(1){
         //take photo    
-        PORTA.0 = 0;
-        delay_ms(200);
         PORTA.0 = 1;
-        //sleep
-        //delay_ms(359800); //360000      6min   
-        delay_ms(800);     //1sec
+        delay_shot();
+        PORTA.0 = 0;
+        delay_minute(unit_count);
+    }
+} else if(PIND.3 == 1){ //hours
+    while(1){
+        //take photo    
+        PORTA.0 = 1;
+        delay_shot();
+        PORTA.0 = 0;
+        delay_hour(unit_count);
+    }
+} else if(PIND.2 == 1){ //days
+    while(1){
+        //take photo    
+        PORTA.0 = 1;
+        delay_shot();
+        PORTA.0 = 0;
+        delay_day(unit_count);
+    }
+}  else {  //seconds
+    while(1){
+        //take photo    
+        PORTA.0 = 1;
+        delay_shot();
+        PORTA.0 = 0;
+        delay_second(unit_count);
+    }
+}
+*/
+
+
+
+    while(1){
+        //physical switch off  
+        PORTA.0 = 0;
+        
+            //Sony IR start
+            shutter();
+            address();
+            delay_ms(11);
+            shutter();
+            address();
+            //Sony IR end
+        
+        //physical switch off
+        delay_ms(200);
+        //physical switch on
+        PORTA.0 = 1;  
+        delay_ms(800);     //1sec 
+        //delay_ms(359800); //360000      6min 
     }
 
 
