0d63f45759ea3976030f675458fe8d5fd7d7b186
minch-yoda <minchenko.dev@gmail.com>
2022-01-31 03:57:00 +0300

    final version for now, everything works as intended, finally added pinout and electronic schematics in pdf format

--- photoshutter.c
+++ photoshutter.c
@@ -6,37 +6,36 @@ ATtiny2313 8 MHz
 
 #include <tiny2313.h>
 #include <delay.h>
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
 const int x0 = 24;
 const int x1 = 48;
-const int ON = 0;  // reversed for both transistor and buttons
-const int OFF = 1;
-int FOREVER = 1;
-float U = 0;
+const int x2 = 96;
+
+int U = 0;
 int BASE = 0;
 int TIMEOUT = 0;
-int RELAY = 0;
-int IR = 1;
-float SUBTRACT = 0;
+int RELAY = 1;
+int CMD = 1;
 //1s/40kHz=25us total (6+19) pulse (microseconds)
 //600us/25us=24 cycles
 void pulse(int cycles){
     int c = 0;
     while(c<cycles){
-        c++;
-        PORTA.0 = ON;
+        c++;         
+        PORTA.0 = 0; //ON  reversed because we use transistor
         delay_us(6);
-        PORTA.0 = OFF;
+        PORTA.0 = 1; //OFF
         delay_us(19);
     }
     delay_us(600);
 }
 
-//1800us*4 + 1200us*3 = 7200+3600 = 10800us = 10.8ms
+//1800us*4 + 1200us*3 +3000 = 7200+3600 +3000 = 13800us = 13.8ms
 void shutter(){
-    pulse(x1); //1800us
+    pulse(x2); //3000us
+	pulse(x1); //1800us
     pulse(x0); //1200us
     pulse(x1); //1800us
     pulse(x1);
@@ -45,26 +44,28 @@ void shutter(){
     pulse(x0);
 }
 
-//1800us*5 + 1200us*2 = 9000+2400 = 11400us = 11.4ms
+//1800us*5 + 1200us*2 +3000 = 9000+2400+3000 = 14400us = 14.4ms
 void shutter2sec(){
+    pulse(x2); //3000us
+	pulse(x1);
     pulse(x1);
     pulse(x1);
     pulse(x0);
-    pulse(x1);
     pulse(x1);
     pulse(x1);
     pulse(x0);
 }
 
-//1800us*2 + 1200us*5 = 3600+6000 = 9600us = 9.6ms
-void videobutton(){
-    pulse(x1);
+//1800us*2 + 1200us*5 +3000 = 3600+6000+3000 = 12600us = 12.6ms
+void video(){
+    pulse(x2); //3000us 
+    pulse(x0);
+    pulse(x0);
+    pulse(x0);
+	pulse(x1);
     pulse(x0);
     pulse(x0);
     pulse(x1);
-    pulse(x0);
-    pulse(x0);
-    pulse(x0);
 }
 
 //1800us*8 + 1200us*5 = 14400+6000 = 20400us = 20.4ms
@@ -104,7 +105,6 @@ CLKPR=0x00;
 //VOID RESET FREE RELAY
 DDRA =0b0001;
 PORTA=0b0011;
-PORTA.0 = OFF;
 
 // Port B
 //All input
@@ -119,59 +119,83 @@ PORTB=0b11111111;
 DDRD=0b00000000;
 PORTD=0b11111111;
 
-delay_ms(200);
+delay_ms(100);
 
-if(PINA.1 == ON){/*doesn't work as input*/}
+//0 means pin is shorted (ON) so we check for false value
+if(!PINA.1){CMD = 2;}
 
-if(PINB.0 == ON){U += 1;}
-if(PINB.1 == ON){U += 2;}
-if(PINB.2 == ON){U += 3;}
-if(PINB.3 == ON){U += 4;}
-if(PINB.4 == ON){U += 5;} 
-if(PINB.5 == ON){U += 10;}
-if(PINB.6 == ON){U += 20;}
-if(PINB.7 == ON){U += 30;}
+if(!PIND.0){BASE = 1000;}      //seconds
+if(!PIND.1){BASE = 60000;}     //minutes 60*1000
+if(!PIND.2){BASE = 3600000;}   //hours 60*60*1000
+if(!PIND.3){BASE = 86400000;}  //days 24*60*60*1000
 
-if(PIND.0 == ON){BASE = 1000;}
-if(PIND.1 == ON){BASE = 60*1000;}
-if(PIND.2 == ON){BASE = 60*60*1000;}
-if(PIND.3 == ON){BASE = 24*60*60*1000;}
-if(PIND.4 == ON){U += 0.25;}
-if(PIND.5 == ON){U += 0.5;}
+if(!PINB.0){U += 1;}
+if(!PINB.1){U += 2;}
+if(!PINB.2){U += 3;}
+if(!PINB.3){U += 4;}
+if(!PINB.4){U += 5;} 
+if(!PINB.5){U += 10;}
+if(!PINB.6){U += 20;}
+if(!PINB.7){U += 30;}
 
-if(PIND.6 == ON){RELAY = 1;}
+TIMEOUT = (BASE * U) - 100;
 
-if(IR){
-    SUBTRACT+= 73.4;
-}
-if(RELAY){
-    SUBTRACT+= 200;
-}
+if(RELAY){TIMEOUT -= 200;}
+if(!PIND.4){TIMEOUT += BASE/4;}   //+0.25
+if(!PIND.5){U += BASE/2;}         //+0.5
 
-TIMEOUT = (int)( ((float)BASE * U)-SUBTRACT );
+if(!PIND.6){CMD = 3;}
 
-while(FOREVER){
-    if(IR){
-        //Sony IR start (73.4 ms total)
-        shutter();
-        address();
-        delay_ms(11);
-        shutter();
-        address();
+
+
+while(1){
+
+        //Sony IR start 
+    switch(CMD){
+        case 1:
+            shutter();
+            address();
+            delay_ms(11);
+            shutter();
+            address();
+            delay_ms(20.6); //100 - 79.4
+            break;
+            
+        case 2:
+            shutter2sec();
+            address();
+            delay_ms(11);
+            shutter2sec();
+            address();
+            delay_ms(19.4); //100 - 80.6
+            break;
+        
+        break;
+        case 3:
+            video();
+            address();
+            delay_ms(11);
+            video();
+            address();
+			delay_ms(23); //100 - 77
+            
+            break;
+            
+        default: break;
     }
     
     if(RELAY){
         //relay
-        PORTA.0 = ON;
+        PORTA.0 = 0;  //ON
         delay_ms(200);
-        PORTA.0 = OFF;  
+        PORTA.0 = 1;  //OFF
     }
 
     
-    if(!TIMEOUT){
-        FOREVER = 0; //fire only this time if no timeout is set
-    } else {
+    if(TIMEOUT){
         delay_ms(TIMEOUT);
+    } else {
+        break; //fire only this time if no timeout is set
     }    
 
 }
